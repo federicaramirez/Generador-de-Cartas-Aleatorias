ff1a1083603c1a413d1157dfad827aa9fe374c9a
federicaramirez <federicaaramirez@gmail.com>
2022-09-29 17:55:37 +0000

    promoviendoElJuego

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generarCartas() {
+    let palos = ["♦", "♥", "♠", "♣"];
+    let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+
+    let paloaleatorio = palos[Math.floor(Math.random() * palos.length)];
+    let numeroaleatorio = number[Math.floor(Math.random() * number.length)];
+
+    document.getElementById("paloarriba").innerHTML = paloaleatorio;
+    document.getElementById("paloabajo").innerHTML = paloaleatorio;
+    document.getElementById("numerocentro").innerHTML = numeroaleatorio;
+  }
+  generarCartas();
 };
